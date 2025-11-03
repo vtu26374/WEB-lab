4b0b0616da212da06313dff271a40b177d8164c4
vtu26374 <vtu26374@veltech.edu.in>
2025-11-03 10:20:03 +0530

    Validation.js

---
+++ Validation.js
@@ -0,0 +1,35 @@
+Validation Javascript(Js)
+
+Var attempt5
+
+function validatelagin(){
+
+var username=document.getElementById("username") No var password=dowment.getElement Byld ("Password").value;
+
+if (username.. "Style Rack" && Password Dressias")
+
+alert("Login successful!);
+
+window.location. "Success-html";
+
+return false;
+
+3cise &
+
+attempt--;
+
+alert("Incorrect"+attempt + "attempt(s) remaining");
+
+if (attempt ==0){
+
+f
+
+document.getElementById("username"). disabled = true;
+
+dowment, gel Element Bytt("Password) disabled: true;
+
+dowment.getElementById("submit")-disabled = true;
+
+return false;
+}
+}
